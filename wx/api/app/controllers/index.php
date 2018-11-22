38fbaecc5bcef086e823a16faf590e95ddc2c52e
ICodeArtist <18257505812@163.com>
2018-11-22 17:26:32 +0800

    登录啥的

--- wx/api/app/controllers/index.php
+++ wx/api/app/controllers/index.php
@@ -14,6 +14,8 @@ class indexController extends grace{
 	public function index(){
 		//系统会自动调用视图 index_index.php
 	}
+	/*******api*******/
+	//获取场景+风格的图片数量
 	public function cjsl(){
 		$cj = $_POST['cj'];
 		$fg = $_POST['fg'];
@@ -21,6 +23,54 @@ class indexController extends grace{
 		$arr = $this->db->where("cj=? and fg=?",array($cj,$fg))->fetch("sl");
 		p(json_encode($arr));
 	}
+	//获取图片参数
+	public function image(){
+		$this->db = db('image');
+		$arr = $this->db->where("cate=? and serial=?",array($_POST['cate'],$_POST['serial']))->fetch("offsetX,offsetY,height,width");
+		p(json_encode($arr));
+	}
+	//登录ojGCH5KgvBHdnbwZJ4r0NtWtB5rI
+	public function login(){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$this->db = db('users');
+		//先判断账户密码的正确性
+		$userinfo = $this->db->where('username=? and password=?',array($username,md5($password)))->fetch();
+		if($userinfo){
+			//获取openid
+			$code = $_POST['code'];
+			$appid = "wxaad4ab73b7b64f99";
+			$appsecret = "8236cfb62a37c9c9d33534fb91bd61a8";
+			$grant_type = "authorization_code";
+			$url='https://api.weixin.qq.com/sns/jscode2session?appid='.$appid.'&secret='.$appsecret.'&js_code='.$code.'&grant_type='.$grant_type;
+			$res = json_decode(file_get_contents($url),true);
+			if($res['openid']){
+				$query = [];
+				if($userinfo['openid']){
+					//判断绑定的openid是否一致
+					if($userinfo['openid'] != $res['openid'])
+						$this->json("","201","该账号和绑定微信号不一致");
+				}else{
+					$userinfo['openid'] = $query['openid'] = $res['openid'];
+				}
+				$query['IPaddress'] = $this->getClientIp();
+				$query['recentlogin'] = date('Y-m-d H:i:s',time());
+				$this->upUserInfo($userinfo['id'],$query);
+				$this->json($userinfo);
+			}else{
+				$this->json("","201","网络错误,请重新打开");
+			}
+		}else{
+			$this->json("","201","请输入正确的账号密码");
+		}
+	}
+
+	public function upUserInfo($uid,$query){
+		$this->db = db('users');
+		$this->db->where('id=?',array($uid))->update($query);
+	}
+	/********工具****/
+	//抓取图片数据
 	public function vrlookshuju(){
 		// $this->db = db('image');
 		// $url = "https://xcx.vrlook.top/shuju.php";
@@ -36,11 +86,7 @@ class indexController extends grace{
 		// }
 		// p($res);
 	}
-	public function image(){
-		$this->db = db('image');
-		$arr = $this->db->where("cate=? and serial=?",array($_POST['cate'],$_POST['serial']))->fetch("offsetX,offsetY,height,width");
-		p(json_encode($arr));
-	}
+	//post请求
 	public function request_post($url = '', $post_data = array()) {
 		if (empty($url) || empty($post_data)) {
 		    return false;
@@ -66,5 +112,20 @@ class indexController extends grace{
 		curl_close($ch);
 
 		return $data;
+	}
+	//获取ipp
+	public function getClientIp() {
+    //strcasecmp 比较两个字符，不区分大小写。返回0，>0，<0。
+    if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+        $ip = getenv('HTTP_CLIENT_IP');
+    } elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+        $ip = getenv('HTTP_X_FORWARDED_FOR');
+    } elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+        $ip = getenv('REMOTE_ADDR');
+    } elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    $res =  preg_match ( '/[\d\.]{7,15}/', $ip, $matches ) ? $matches [0] : '';
+		return $res;
 	}
 }
